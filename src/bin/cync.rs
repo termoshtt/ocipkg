0da3ec10d742c83c6e8df66bbf43fedcf8a78750
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-06-09 02:49:23 +0900

    Use anyhow::Result

--- src/bin/cync.rs
+++ src/bin/cync.rs
@@ -27,7 +27,7 @@ fn calc_digest(buf: &[u8]) -> String {
     digest
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let Opt {
         input_directory,
         mut output,
@@ -41,58 +41,33 @@ fn main() {
         );
     }
 
-    let mut ar = tar::Builder::new(Vec::new());
-    ar.append_dir_all("rootfs-c9d-v1", &input_directory)
-        .expect("Failed to create tar archive");
-    let buf = ar.into_inner().unwrap();
-
-    output.set_extension("tar");
-    let mut out = fs::File::create(output).unwrap();
-    out.write_all(&buf).unwrap();
-
-    let digest = calc_digest(&buf);
-    dbg!(digest);
-
     let config = DescriptorBuilder::default()
         .media_type(MediaType::ImageConfig)
         .size(7023)
         .digest("sha256:b5b2b2c507a0944348e0303114d8d93aaaa081732b86451d9bce1f432a537bc7")
-        .build()
-        .expect("build config descriptor");
+        .build()?;
 
-    let layers: Vec<Descriptor> = [
-        (
-            32654,
-            "sha256:9834876dcfb05cb167a5c24953eba58c4ac89b1adf57f28f2f9d09af107ee8f0",
-        ),
-        (
-            16724,
-            "sha256:3c3a4604a545cdc127456d94e421cd355bca5b528f4a9c1905b15da2eb4a4c6b",
-        ),
-        (
-            73109,
-            "sha256:ec4b8955958665577945c89419d1af06b5f7636b4ac3da7f12184802ad867736",
-        ),
-    ]
-    .iter()
-    .map(|l| {
-        DescriptorBuilder::default()
-            .media_type(MediaType::ImageLayerGzip)
-            .size(l.0)
-            .digest(l.1.to_owned())
-            .build()
-            .expect("build layer")
-    })
-    .collect();
+    let mut ar = tar::Builder::new(Vec::new());
+    ar.append_dir_all("rootfs-c9d-v1", &input_directory)?;
+    let buf: Vec<u8> = ar.into_inner()?;
+
+    output.set_extension("tar");
+    let mut out = fs::File::create(output)?;
+    out.write_all(&buf)?;
+
+    let layers: Vec<Descriptor> = vec![DescriptorBuilder::default()
+        .media_type(MediaType::ImageLayer)
+        .size(buf.len() as i64)
+        .digest(calc_digest(&buf))
+        .build()?];
 
     let image_manifest = ImageManifestBuilder::default()
         .schema_version(SCHEMA_VERSION)
         .config(config)
         .layers(layers)
-        .build()
-        .expect("build image manifest");
+        .build()?;
 
-    image_manifest
-        .to_writer_pretty(&mut std::io::stdout())
-        .unwrap();
+    image_manifest.to_writer_pretty(&mut std::io::stdout())?;
+
+    Ok(())
 }
