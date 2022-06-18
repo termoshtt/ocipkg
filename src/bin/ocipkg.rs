5238e80c007f298a983e0d0ca2654f9c1cc0af21
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-06-18 23:20:25 +0900

    Subcommand initialize and pack

---
+++ src/bin/ocipkg.rs
@@ -0,0 +1,42 @@
+use log::info;
+use std::{fs, path::PathBuf};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ocipkg", about = "OCI Registry for binary distribution")]
+enum Opt {
+    /// Pack a directory into an oci-archive tar file
+    Pack {
+        /// Input directory
+        #[structopt(parse(from_os_str))]
+        input_directory: PathBuf,
+
+        /// Output oci archive
+        #[structopt(parse(from_os_str))]
+        output: PathBuf,
+    },
+
+    /// Initialize local storage
+    Initialize {},
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+    match Opt::from_args() {
+        Opt::Pack {
+            input_directory,
+            output,
+        } => {
+            ocipkg::compose::compose(&input_directory, &output)?;
+        }
+
+        Opt::Initialize {} => {
+            let dir = ocipkg::config::data_dir()?;
+            if !dir.is_dir() {
+                info!("Create directory: {}", dir.display());
+                fs::create_dir_all(&dir)?;
+            }
+        }
+    }
+    Ok(())
+}
