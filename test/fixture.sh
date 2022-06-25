b8d3efd2f5039320f8a5cbfefa9a3b98d07c9b84
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-06-26 00:34:31 +0900

    Use SCRIPT_DIR

--- test/fixture.sh
+++ test/fixture.sh
@@ -7,9 +7,11 @@ set -eu
 REGISTRY="localhost:5000"
 REPO_NAME="test_repo"
 
+SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE:-$0}))
+
 curl -f -X GET http://${REGISTRY}/v2
 
 for tag in tag1 tag2 tag3; do
-  docker build -t ${REGISTRY}/${REPO_NAME}:${tag} .
+  docker build -t ${REGISTRY}/${REPO_NAME}:${tag} ${SCRIPT_DIR}
   docker push ${REGISTRY}/${REPO_NAME}:${tag}
 done
