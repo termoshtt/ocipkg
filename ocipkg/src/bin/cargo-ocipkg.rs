7840db531ebbe0659c83f4797752ade3f829d648
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-02 00:06:43 +0900

    Get cargo metadata

--- ocipkg/src/bin/cargo-ocipkg.rs
+++ ocipkg/src/bin/cargo-ocipkg.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+use cargo_metadata::{Metadata, MetadataCommand};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -13,13 +15,30 @@ enum Opt {
     Publish {},
 }
 
-fn main() {
+fn get_metadata() -> anyhow::Result<Metadata> {
+    let mut args = std::env::args().skip_while(|val| !val.starts_with("--manifest-path"));
+    let mut cmd = MetadataCommand::new();
+    match args.next() {
+        Some(ref p) if p == "--manifest-path" => {
+            cmd.manifest_path(args.next().context("Manifest path not found")?);
+        }
+        Some(p) => {
+            cmd.manifest_path(p.trim_start_matches("--manifest-path="));
+        }
+        None => {}
+    };
+    let metadata = cmd.exec()?;
+    Ok(metadata)
+}
+
+fn main() -> anyhow::Result<()> {
     match Opt::from_args() {
         Opt::Build { release } => {
-            dbg!(release);
+            dbg!(get_metadata()?, release);
             todo!("cargo-ocipkg build")
         }
         Opt::Publish {} => {
+            dbg!(get_metadata()?);
             todo!("cargo-ocipkg publish")
         }
     }
