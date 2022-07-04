3b40f3f334f11889a533f9042c299dd371f5b310
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-04 18:20:45 +0900

    Use ImageName for image_dir

--- ocipkg/src/distribution/mod.rs
+++ ocipkg/src/distribution/mod.rs
@@ -14,19 +14,11 @@ use oci_spec::image::*;
 /// Get image from registry and save it into local storage
 pub async fn get_image(image_name: &ImageName) -> anyhow::Result<()> {
     let ImageName {
-        name,
-        hostname,
-        reference,
-        ..
+        name, reference, ..
     } = image_name;
     let client = Client::new(&image_name.registry_url()?, name)?;
     let manifest = client.get_manifest(reference).await?;
-    let dest = crate::config::image_dir(&format!(
-        "{}/{}/__{}",
-        hostname,
-        name.as_str(),
-        reference.as_str()
-    ))?;
+    let dest = crate::config::image_dir(&image_name)?;
     for layer in manifest.layers() {
         let blob = client.get_blob(layer.digest()).await?;
         match layer.media_type() {
