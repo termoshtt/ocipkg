b1737220f27c44810703b02529e9b489252f82fc
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-08 17:06:16 +0900

    Moc ocipkg::find_package

--- ocipkg-exe-example/build.rs
+++ ocipkg-exe-example/build.rs
@@ -1,1 +1,4 @@
-fn main() {}
+fn main() -> anyhow::Result<()> {
+    ocipkg::find_package("localhost:5000/test_repo:tag1")?;
+    Ok(())
+}
--- ocipkg/src/lib.rs
+++ ocipkg/src/lib.rs
@@ -10,3 +10,9 @@ mod image_name;
 
 pub use digest::Digest;
 pub use image_name::ImageName;
+
+pub fn find_package(image_name: &str) -> anyhow::Result<()> {
+    let image_name = ImageName::parse(image_name)?;
+    dbg!(image_name);
+    Ok(())
+}
