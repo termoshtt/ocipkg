05f2290c80b096595c20990f7d6b04bab2b22e73
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-09 17:41:08 +0900

    Implement link_package

--- ocipkg/src/lib.rs
+++ ocipkg/src/lib.rs
@@ -11,6 +11,8 @@ mod image_name;
 pub use digest::Digest;
 pub use image_name::ImageName;
 
+use std::fs;
+
 /// Get and link package to current crate using [cargo link instructions](https://doc.rust-lang.org/cargo/reference/build-scripts.html#outputs-of-the-build-script).
 ///
 /// This is aimed to use in [build script](https://doc.rust-lang.org/cargo/reference/build-scripts.html) a.k.a. `build.rs`.
@@ -22,5 +24,31 @@ pub fn link_package(image_name: &str) -> anyhow::Result<()> {
             .await
             .expect("Failed to get image");
     });
+    let dir = config::image_dir(&image_name)?;
+    println!("cargo:rustc-link-search={}", dir.display());
+    for entry in fs::read_dir(&dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            let name = path
+                .file_stem()
+                .unwrap()
+                .to_str()
+                .expect("Non UTF-8 is not supported");
+            let name = if let Some(name) = name.strip_prefix("lib") {
+                name
+            } else {
+                continue;
+            };
+            if let Some(ext) = path.extension() {
+                if ext == "a" {
+                    println!("cargo:rustc-link-lib=static={}", name);
+                }
+                if ext == "so" {
+                    println!("cargo:rustc-link-lib=dylib={}", name);
+                }
+            }
+        }
+    }
     Ok(())
 }
