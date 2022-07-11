c035a3b001fa792fae3f1bca35dbbf531447701c
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-11 15:05:56 +0900

    Invalid inputs

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,15 @@
 pub enum Error {
     #[error("No valid home directory path could be retrieved from the operating system.")]
     NoValidHomeDirecotry,
+
+    #[error("Invalid digest: {0}")]
+    InvalidDigest(String),
+
+    #[error("Invalid name for repository: {0}")]
+    InvalidName(String),
+
+    #[error("Invalid reference to image: {0}")]
+    InvalidReference(String),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
