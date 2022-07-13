71424344c46ba8cfb4fa6376b916bb3127968810
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-13 15:50:44 +0900

    from_target_triple

--- src/image/image_configuration.rs
+++ src/image/image_configuration.rs
@@ -2,7 +2,7 @@ use oci_spec::image::*;
 
 pub trait PlatformEx: Sized {
     fn from_cargo_cfg() -> Self;
-    fn from_target_triple() -> anyhow::Result<Self>;
+    fn from_target_triple(target_triple: &str) -> anyhow::Result<Self>;
 }
 
 impl PlatformEx for Platform {
@@ -30,7 +30,27 @@ impl PlatformEx for Platform {
         builder.build().unwrap()
     }
 
-    fn from_target_triple() -> anyhow::Result<Self> {
-        todo!()
+    fn from_target_triple(target_triple: &str) -> anyhow::Result<Self> {
+        let parts: Vec<&str> = target_triple.split('-').collect();
+        let (arch, os) = match parts[..] {
+            [arch, _vender, os, _env] => (arch, os),
+            [arch, os, _env] => (arch, os),
+            _ => anyhow::bail!("Unknown target triple: {}", target_triple),
+        };
+        let (arch, variant) = match arch {
+            "x86_64" => (Arch::Amd64, None),
+            "aarch64" => (Arch::ARM64, Some("v8".to_string())),
+            _ => anyhow::bail!("Unsupported arch: {}", arch),
+        };
+        let os = match os {
+            "linux" => Os::Linux,
+            "windows" => Os::Windows,
+            _ => anyhow::bail!("Unsupported OS: {}", os),
+        };
+        let mut builder = PlatformBuilder::default().os(os).architecture(arch);
+        if let Some(variant) = variant {
+            builder = builder.variant(variant);
+        }
+        Ok(builder.build().unwrap())
     }
 }
