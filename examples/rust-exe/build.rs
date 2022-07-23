f5ee254d717170d1a6be372ab0893026fb4898bd
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-07-24 02:10:26 +0900

    Split static and dynamic library example

--- examples/rust-exe/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    ocipkg::link_package("ghcr.io/termoshtt/ocipkg/rust-lib:latest")
-        .expect("Failed to link rust-lib");
-}
---
+++ examples/static/rust/exe/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    ocipkg::link_package("ghcr.io/termoshtt/ocipkg/static/rust:0dbc47b").unwrap();
+}
