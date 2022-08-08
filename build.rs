ea980c1bc7f4bf5daae91e5590f3e61eb6915a58
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-08-08 18:52:55 +0900

    Make vergen dependency optional

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
+#[cfg(feature = "cli")]
 use vergen::{vergen, Config};
 
 fn main() {
-    let mut cfg = Config::default();
-    *cfg.sysinfo_mut().name_mut() = false;
-    vergen(cfg).expect("Fail to generate version info");
+    #[cfg(feature = "cli")]
+    {
+        let mut cfg = Config::default();
+        *cfg.sysinfo_mut().name_mut() = false;
+        vergen(cfg).expect("Fail to generate version info");
+    }
 }
