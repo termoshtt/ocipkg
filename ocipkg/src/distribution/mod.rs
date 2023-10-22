a231a22a9b8274c440df8c53e3b40e7fa94d4787
Cody Taylor <codemister99@yahoo.com>
2023-10-22 03:33:09 +0000

    Image: Helper to get data blob of a specific layer

--- ocipkg/src/distribution/mod.rs
+++ ocipkg/src/distribution/mod.rs
@@ -43,31 +43,34 @@ pub fn push_image(path: &Path) -> Result<()> {
 
 /// Get image from registry and save it into local storage
 pub fn get_image(image_name: &ImageName) -> Result<()> {
-    let ImageName {
-        name, reference, ..
-    } = image_name;
-    let mut client = Client::new(image_name.registry_url()?, name.clone())?;
-    let manifest = client.get_manifest(reference)?;
-    let maybe_layer = manifest.layers().iter().find(|&d| {
-        match d.media_type() {
+    let blob = get_layer_bytes(&image_name, |media_type| {
+        match media_type {
             MediaType::ImageLayerGzip => true,
             // application/vnd.docker.image.rootfs.diff.tar.gzip case
             MediaType::Other(ty) if ty.ends_with("tar.gzip") => true,
             _ => false,
         }
-    });
+    })?;
+    let buf = flate2::read::GzDecoder::new(blob.as_slice());
+    let dest = crate::local::image_dir(image_name)?;
 
-    match maybe_layer {
-        Some(layer) => {
-            let blob = client.get_blob(&Digest::new(layer.digest())?)?;
-            let buf = flate2::read::GzDecoder::new(blob.as_slice());
-            let dest = crate::local::image_dir(image_name)?;
+    log::info!("Get {} into {}", image_name, dest.display());
+    tar::Archive::new(buf).unpack(dest)?;
 
-            log::info!("Get {} into {}", image_name, dest.display());
-            tar::Archive::new(buf).unpack(dest)?;
+    Ok(())
+}
 
-            Ok(())
-        },
-        None => Err(Error::MissingLayer),
-    }
+/// Get the data blob of a specific image layer, filtering by media_type.
+pub fn get_layer_bytes(image_name: &ImageName, f: impl Fn(&MediaType) -> bool) -> Result<Vec<u8>> {
+    let registry_url = image_name.registry_url()?;
+    let mut client = Client::new(registry_url, image_name.name.clone())?;
+    let manifest = client.get_manifest(&image_name.reference)?;
+    let layer =
+        manifest.layers()
+        .iter()
+        .find(|&d| f(d.media_type()))
+        .ok_or(Error::MissingLayer)?;
+    let digest = Digest::new(layer.digest())?;
+
+    client.get_blob(&digest)
 }
