4b2efbc037762355c8b2d7202a18841c9ebc962f
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-16 00:01:16 +0900

    impl ArtifactWriter for LocalArtifact

--- ocipkg/src/image/artifact.rs
+++ ocipkg/src/image/artifact.rs
@@ -1,7 +1,6 @@
-use crate::{error::*, local::image_dir, ImageName};
-use chrono::Local;
-use oci_spec::image::{self, Descriptor, MediaType};
-use std::fs;
+use crate::{error::*, local::image_dir, Digest, ImageName};
+use oci_spec::image::{Descriptor, ImageManifest, ImageManifestBuilder, MediaType};
+use std::{fs, path::PathBuf};
 
 /// Writer trait based on [Guidelines for Artifact Usage](https://github.com/opencontainers/image-spec/blob/v1.1.0/manifest.md#guidelines-for-artifact-usage) of OCI image specification 1.1.0
 pub trait ArtifactWriter {
@@ -19,6 +18,8 @@ pub trait ArtifactWriter {
 
 pub struct LocalArtifact {
     image_name: ImageName,
+    manifest: ImageManifest,
+    oci_dir_root: PathBuf,
 }
 
 impl Drop for LocalArtifact {
@@ -35,11 +36,54 @@ impl LocalArtifact {
         if path.exists() {
             return Err(Error::ImageAlreadyExists(path));
         }
-        Ok(Self { image_name })
+        let oci_dir_root = image_name.local_path()?.join(".oci-dir");
+        fs::create_dir_all(&oci_dir_root)?;
+        Ok(Self {
+            image_name,
+            manifest: ImageManifestBuilder::default().build()?,
+            oci_dir_root,
+        })
     }
 
     fn cleanup(&self) -> Result<()> {
         Ok(fs::remove_dir_all(self.image_name.local_path()?)?)
+    }
+}
+
+impl ArtifactWriter for LocalArtifact {
+    fn add_blob(&mut self, descriptor: Descriptor, blob: &[u8]) -> Result<()> {
+        let digest = Digest::new(&descriptor.digest())?;
+        // FIXME: check digest of blob
+        let path = self.oci_dir_root.join(digest.as_path());
+        fs::write(path, blob)?;
+        self.manifest.layers_mut().push(descriptor);
+        Ok(())
+    }
+
+    fn add_config(&mut self, descriptor: Descriptor, config: &[u8]) -> Result<()> {
+        let digest = Digest::new(&descriptor.digest())?;
+        // FIXME: check digest of config
+        let path = self.oci_dir_root.join(digest.as_path());
+        fs::write(path, config)?;
+        self.manifest.set_config(descriptor);
+        Ok(())
+    }
+
+    fn add_annotation(&mut self, key: &str, value: &str) -> Result<()> {
+        self.manifest
+            .annotations_mut()
+            .get_or_insert(Default::default())
+            .insert(key.to_string(), value.to_string());
+        Ok(())
+    }
+
+    fn set_artifact_type(&mut self, artifact_type: MediaType) -> Result<()> {
+        self.manifest.set_artifact_type(Some(artifact_type));
+        Ok(())
+    }
+
+    fn finish(self) -> Result<()> {
+        todo!()
     }
 }
 
