a097b4c8e867d90c3d9e8f1189890645691f2d76
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-16 00:26:07 +0900

    ArtifactWriter::{add_file, add_directory}

--- ocipkg/src/image/artifact.rs
+++ ocipkg/src/image/artifact.rs
@@ -1,17 +1,40 @@
 use crate::{error::*, local::image_dir, Digest, ImageName};
 use oci_spec::image::{Descriptor, ImageManifest, ImageManifestBuilder, MediaType};
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 /// Writer trait based on [Guidelines for Artifact Usage](https://github.com/opencontainers/image-spec/blob/v1.1.0/manifest.md#guidelines-for-artifact-usage) of OCI image specification 1.1.0
 pub trait ArtifactWriter {
-    /// Add a blob with media-type to the image. This blob is added to layers.
+    /// Add a file to the artifact.
+    ///
+    /// - The file is compressed by gzip, and added in OCI artifact as a layer.
+    /// - Its media type is set as `application/vnd.ocipkg.file+gzip`
+    /// - On local storage, the file is stored at the top of image directory.
+    fn add_file(&mut self, file: &Path) -> Result<()>;
+
+    /// Add a directory to the artifact.
+    ///
+    /// - The directory is archived by tar and compressed by gzip, and then added in OCI artifact as a layer.
+    /// - Its media type is set as `application/vnd.ocipkg.directory.tar+gzip`
+    /// - On local storage, the directory is stored at the top of image directory.
+    fn add_directory(&mut self, directory: &Path) -> Result<()>;
+
+    /// Add a blob with arbitrary descriptor to the image.
     fn add_blob(&mut self, descriptor: Descriptor, blob: &[u8]) -> Result<()>;
+
     /// Add a config to the image manifest
+    ///
+    /// The guideline of OCI artifact has three cases,
     fn add_config(&mut self, descriptor: Descriptor, config: &[u8]) -> Result<()>;
+
     /// Add an annotation to the image manifest
     fn add_annotation(&mut self, key: &str, value: &str) -> Result<()>;
+
     /// Set `artifactType` field in the manifest
     fn set_artifact_type(&mut self, artifact_type: MediaType) -> Result<()>;
+
     /// Finish writing the image
     fn finish(self) -> Result<()>;
 }
@@ -51,6 +74,18 @@ impl LocalArtifact {
 }
 
 impl ArtifactWriter for LocalArtifact {
+    fn add_file(&mut self, file: &Path) -> Result<()> {
+        let bytes: Vec<u8> = todo!();
+        let descriptor: Descriptor = todo!();
+        self.add_blob(descriptor, &bytes)
+    }
+
+    fn add_directory(&mut self, directory: &Path) -> Result<()> {
+        let bytes: Vec<u8> = todo!();
+        let descriptor: Descriptor = todo!();
+        self.add_blob(descriptor, &bytes)
+    }
+
     fn add_blob(&mut self, descriptor: Descriptor, blob: &[u8]) -> Result<()> {
         let digest = Digest::new(&descriptor.digest())?;
         // FIXME: check digest of blob
