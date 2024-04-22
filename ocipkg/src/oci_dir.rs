201ee9d58c42284fac8219eeb02c41dfa1faba11
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-23 00:39:28 +0900

    Remove oci-dir if it is not finished

--- ocipkg/src/oci_dir.rs
+++ ocipkg/src/oci_dir.rs
@@ -10,10 +10,27 @@ use std::{fs, path::PathBuf};
 
 /// Builder for `.oci-dir` directory
 ///
-/// This is responsible for saving any data and manifest files as blobs, and create `index.json` file.
+/// This is responsible for saving any data and manifest files as blobs, and creating `index.json` file.
+/// Creating manifest is out of scope of this struct.
 ///
 pub struct OciDirBuilder {
     oci_dir_root: PathBuf,
+    is_finished: bool,
+}
+
+impl Drop for OciDirBuilder {
+    fn drop(&mut self) {
+        // Remove oci-dir if it is not finished.
+        if !self.is_finished {
+            fs::remove_dir_all(&self.oci_dir_root).unwrap_or_else(|e| {
+                log::error!(
+                    "Failed to remove oci-dir {}: {}",
+                    self.oci_dir_root.display(),
+                    e
+                )
+            });
+        }
+    }
 }
 
 impl OciDirBuilder {
@@ -22,9 +39,13 @@ impl OciDirBuilder {
             return Err(Error::ImageAlreadyExists(oci_dir_root));
         }
         fs::create_dir_all(&oci_dir_root)?;
-        Ok(Self { oci_dir_root })
+        Ok(Self {
+            oci_dir_root,
+            is_finished: false,
+        })
     }
 
+    /// Save binary data as a blob file.
     pub fn save_blob(&self, data: &[u8]) -> Result<Digest> {
         let digest = Digest::from_buf_sha256(data);
         let out = self.oci_dir_root.join(digest.as_path());
@@ -36,7 +57,7 @@ impl OciDirBuilder {
     /// Create `index.json` file with image manifest.
     ///
     /// Although `index.json` can store multiple manifests, this API does not support it.
-    pub fn finish(self, manifest: ImageManifest) -> Result<()> {
+    pub fn finish(mut self, manifest: ImageManifest) -> Result<()> {
         let manifest_json = serde_json::to_string(&manifest)?;
         let digest = self.save_blob(manifest_json.as_bytes())?;
         let descriptor = DescriptorBuilder::default()
@@ -51,6 +72,7 @@ impl OciDirBuilder {
             self.oci_dir_root.join("index.json"),
             serde_json::to_string(&index)?,
         )?;
+        self.is_finished = true;
         Ok(())
     }
 }
