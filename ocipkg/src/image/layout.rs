27b4da9b7bc31bcf645ae432544f98d1c0015992
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-30 15:11:49 +0900

    Check `org.opencontainers.image.ref.name` annotation

--- ocipkg/src/image/layout.rs
+++ ocipkg/src/image/layout.rs
@@ -18,11 +18,23 @@ pub trait ImageLayout {
     /// Get manifest stored in the image layout.
     ///
     /// Note that this trait assumes a single manifest in a single layout.
-    fn get_manifest(&mut self) -> Result<ImageManifest> {
+    /// If `index.json` contains `org.opencontainers.image.ref.name` annotation, it is returned as [ImageName].
+    fn get_manifest(&mut self) -> Result<(Option<ImageName>, ImageManifest)> {
         let index = self.get_index()?;
-        let digest =
-            Digest::from_descriptor(index.manifests().first().context("Missing manifest")?)?;
-        Ok(serde_json::from_slice(self.get_blob(&digest)?.as_slice())?)
+        let desc = index.manifests().first().context("Missing manifest")?;
+        let name = if let Some(name) = desc
+            .annotations()
+            .as_ref()
+            .and_then(|annotations| annotations.get("org.opencontainers.image.ref.name"))
+        {
+            // Invalid image name raises an error, while missing name is just ignored.
+            Some(ImageName::parse(name)?)
+        } else {
+            None
+        };
+        let digest = Digest::from_descriptor(desc)?;
+        let manifest = serde_json::from_slice(self.get_blob(&digest)?.as_slice())?;
+        Ok((name, manifest))
     }
 }
 
