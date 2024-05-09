d1ad5a0973641fa60eddaa5c31a1d34a626651e7
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-09 18:43:10 +0900

    Use image::copy in distribution::push_image

--- ocipkg/src/distribution/mod.rs
+++ ocipkg/src/distribution/mod.rs
@@ -11,34 +11,19 @@ pub use name::Name;
 pub use oci_spec::image::MediaType;
 pub use reference::Reference;
 
-use crate::{Digest, ImageName};
+use crate::{
+    image::{copy, Image, OciArchive, RemoteBuilder},
+    Digest, ImageName,
+};
 use anyhow::{bail, Result};
 use std::{fs, io::Read, path::Path};
 
 /// Push image to registry
 pub fn push_image(path: &Path) -> Result<()> {
-    if !path.is_file() {
-        bail!("{} is not a file", path.display());
-    }
-    let mut f = fs::File::open(path)?;
-    let mut ar = crate::image::Archive::new(&mut f);
-    for (image_name, manifest) in ar.get_manifests()? {
-        log::info!("Push image: {}", image_name);
-        let mut client = Client::new(image_name.registry_url()?, image_name.name)?;
-        for layer in manifest.layers() {
-            let digest = Digest::new(layer.digest())?;
-            let mut entry = ar.get_blob(&digest)?;
-            let mut buf = Vec::new();
-            entry.read_to_end(&mut buf)?;
-            client.push_blob(&buf)?;
-        }
-        let digest = Digest::new(manifest.config().digest())?;
-        let mut entry = ar.get_blob(&digest)?;
-        let mut buf = Vec::new();
-        entry.read_to_end(&mut buf)?;
-        client.push_blob(&buf)?;
-        client.push_manifest(&image_name.reference, &manifest)?;
-    }
+    let mut oci_archive = OciArchive::new(path)?;
+    let image_name = oci_archive.get_name()?;
+    let remote = RemoteBuilder::new(image_name)?;
+    copy(oci_archive, remote)?;
     Ok(())
 }
 
