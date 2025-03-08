323939d0b772239544cf4dda40f2f8f3f365dd88
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-03-08 17:55:29 +0900

    Add executable

--- ocipkg/src/image/runnable.rs
+++ ocipkg/src/image/runnable.rs
@@ -4,14 +4,15 @@ use std::path::PathBuf;
 
 use super::OciArchiveBuilder;
 use crate::{image::ImageBuilder, ImageName};
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use oci_spec::image::{ConfigBuilder, DescriptorBuilder, ImageManifestBuilder};
 
 /// Build [`Runnable`], executable container
 pub struct RunnableBuilder<LayoutBuilder: ImageBuilder> {
     manifest: ImageManifestBuilder,
-    config: ConfigBuilder,
+    entrypoint: Vec<String>,
     layout: LayoutBuilder,
+    layers: Vec<oci_spec::image::Descriptor>,
 }
 
 impl<LayoutBuilder: ImageBuilder> RunnableBuilder<LayoutBuilder> {
@@ -19,19 +20,56 @@ impl<LayoutBuilder: ImageBuilder> RunnableBuilder<LayoutBuilder> {
         Ok(Self {
             layout: builder,
             manifest: ImageManifestBuilder::default().schema_version(2_u32),
-            config: ConfigBuilder::default(),
+            entrypoint: Vec::new(),
+            layers: Vec::new(),
         })
     }
 
     pub fn append_executable(&mut self, path: &PathBuf) -> Result<()> {
-        // FIXME
-        dbg!(path);
+        if !path.is_file() {
+            anyhow::bail!("File does not exist: {:?}", path);
+        }
+        if !self.layers.is_empty() {
+            anyhow::bail!("Only one executable is allowed");
+        }
+
+        let filename = path
+            .file_name()
+            .unwrap() // Checked above
+            .to_str()
+            .expect("Non-UTF8 filename");
+
+        let mut buf = Vec::new();
+        {
+            let mut tar_builder = tar::Builder::new(&mut buf);
+            let mut file = std::fs::File::open(path)?;
+            tar_builder.append_file(filename, &mut file)?;
+            log::info!("Put executable `/{}` into container", filename);
+        }
+
+        let (digest, size) = self.layout.add_blob(&buf)?;
+
+        let layer_desc = DescriptorBuilder::default()
+            .media_type(oci_spec::image::MediaType::ImageLayer)
+            .size(size)
+            .digest(digest)
+            .build()?;
+        self.layers.push(layer_desc);
+
+        self.entrypoint.push(format!("/{filename}"));
 
         Ok(())
     }
 
     pub fn build(mut self) -> Result<Runnable<LayoutBuilder::Image>> {
-        let config = self.config.build()?;
+        ensure!(
+            !self.layers.is_empty() && !self.entrypoint.is_empty(),
+            "No executable provided. Use `append_executable` to add one"
+        );
+
+        let config = ConfigBuilder::default()
+            .entrypoint(self.entrypoint)
+            .build()?;
         let cfg_json = serde_json::to_string(&config)?;
         let (digest, size) = self.layout.add_blob(cfg_json.as_bytes())?;
         let cfg_desc = DescriptorBuilder::default()
@@ -40,10 +78,7 @@ impl<LayoutBuilder: ImageBuilder> RunnableBuilder<LayoutBuilder> {
             .digest(digest)
             .build()?;
 
-        // FIXME
-        let layers = Vec::new();
-
-        let manifest = self.manifest.config(cfg_desc).layers(layers).build()?;
+        let manifest = self.manifest.config(cfg_desc).layers(self.layers).build()?;
         Ok(Runnable(self.layout.build(manifest)?))
     }
 }
