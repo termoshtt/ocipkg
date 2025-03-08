88930a1cdcd90123b852906cb2761a4d86c93686
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-03-08 22:44:42 +0900

    Test for argument

--- examples/static-exe/src/main.rs
+++ examples/static-exe/src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let name = if args.len() > 1 { &args[1] } else { "World" };
+    println!("Hello, {}!", name);
 }
